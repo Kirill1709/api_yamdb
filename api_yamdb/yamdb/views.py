cea5108d47ac87c6bb0c3f3921a16478170cb657
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-07-22 16:14:37 +0300

    Fix import

--- api_yamdb/yamdb/views.py
+++ api_yamdb/yamdb/views.py
@@ -2,12 +2,11 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import filters, serializers, status, viewsets
+from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
